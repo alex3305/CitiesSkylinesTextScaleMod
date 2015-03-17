9a4d51e10d26238ed2e8e8bd5242507380bff44d
Alex van den Hoogen <alex.van.den.hoogen@geodan.nl>
2015-03-17 22:01:32 +0100

    Fixes text overflow issues for ingame text.

--- CitiesLargeText/Scaling.cs
+++ CitiesLargeText/Scaling.cs
@@ -4,7 +4,34 @@
     using ICities;
     using UnityEngine;
 
-    public class Scaling : LoadingExtensionBase {
+    public class Scaling {
+
+        private static Scaling instance;
+
+        public static Scaling Instance {
+            get {
+                return (instance = instance ?? new Scaling());
+            }
+        }
+
+        public UILabel[] Labels { get; set; }
+
+        public void UpdateUI(UILabel[] labels) {
+            Labels = labels;
+
+            foreach (var label in Labels) {
+                if (label.textScale != ModInfo.Configuration.Scale) {
+                    label.textScale = ModInfo.Configuration.Scale;
+                    label.clipChildren = false;
+                    label.size = new Vector2(label.size.x, label.size.y * ModInfo.Configuration.Scale);
+                    label.Invalidate();
+                }
+            }
+        }
+
+    }
+
+    public class InitialScaling : LoadingExtensionBase {
 
         public override void OnLevelLoaded(LoadMode mode) {
             base.OnLevelLoaded(mode);
@@ -12,13 +39,20 @@
             var uiView = GameObject.FindObjectOfType<UIView>();
             if (uiView == null) return;
 
-            foreach (var label in GameObject.FindObjectsOfType<UILabel>()) {
-                if (label.textScale != ModInfo.Configuration.Scale) {
-                    label.textScale = ModInfo.Configuration.Scale;
-                    label.clipChildren = false;
-                    label.Invalidate();
-                }
-            }
+            Scaling.Instance.UpdateUI(GameObject.FindObjectsOfType<UILabel>());
         }
+    }
+
+    public class ContinuesScaling : ThreadingExtensionBase {
+
+        public override void OnUpdate(float realTimeDelta, float simulationTimeDelta) {
+            var labels = GameObject.FindObjectsOfType<UILabel>();
+            if (Scaling.Instance.Labels != labels) {
+                Scaling.Instance.UpdateUI(labels);
+            }
+
+            base.OnUpdate(realTimeDelta, simulationTimeDelta);
+        }
+
     }
 }
